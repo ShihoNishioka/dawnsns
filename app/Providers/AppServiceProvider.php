43fa8e50f929064e59f4c4fad8173c65dc27ec6b
LAPTOP-QGFA1O0Q\baata <baataacoo@gmail.com>
2023-05-20 16:41:51 +0900

    フォロー・フォロワー数の表示_20230520

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer('*', function ($view)
+        {
+            $view->with('countFollower', DB::table('follows')->where('follower',Auth::id())->count());
+            $view->with('countFollow', DB::table('follows')->where('follow',Auth::id())->count());
+        });
     }
 }
