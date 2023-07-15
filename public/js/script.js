0c705c095bfed2da80d41b3c3f3438135f65326d
LAPTOP-QGFA1O0Q\baata <baataacoo@gmail.com>
2023-07-15 15:51:44 +0900

    モーダルウィンドウの作成_20230630

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,9 @@
 $(function () { //①
   $('.modal-open').each(function () {
     $(this).on('click', function () {
+      //data-target="post-modal{{ $post-id }}"を取得し、
       var target = $(this).data('target');
+      //class="updateModal"の{{ $post->id }}を表示させる指示
       var modal = document.getElementById(target);
       console.log(modal);
       $(modal).fadeIn();
@@ -9,7 +11,7 @@ $(function () { //①
     });
   });
   $('.modalClose').on('click', function () {
-    $('.js-modal').fadeOut();
+    $('.postModal').fadeOut();
     return false;
   });
 });
